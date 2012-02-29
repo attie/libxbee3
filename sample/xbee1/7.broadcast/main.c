e75b877c32b50400119d5ea7325730bdee362b9f
Attie Grande <attie@attie.co.uk>
2012-02-29 22:52:40 +0000

    added xbee1 broadcast example

---
+++ sample/xbee1/7.broadcast/main.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <xbee.h>
+
+void myCB(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, void **data) {
+	if ((*pkt)->dataLen > 0) {
+		if ((*pkt)->data[0] == '@') {
+			xbee_conCallbackSet(con, NULL, NULL);
+			printf("*** DISABLED CALLBACK... ***\n");
+		}
+		printf("rx: [%s]\n", (*pkt)->data);
+	}
+	printf("tx: %d\n", xbee_conTx(con, NULL, "Hello\r\n"));
+}
+
+int main(void) {
+	int i;
+	void *d;
+	struct xbee *xbee;
+	struct xbee_con *con;
+	struct xbee_conAddress address;
+	struct xbee_conSettings settings;
+	xbee_err ret;
+
+	if ((ret = xbee_setup(&xbee, "xbee1", "/dev/ttyUSB0", 57600)) != XBEE_ENONE) {
+		printf("ret: %d (%s)\n", ret, xbee_errorToStr(ret));
+		return ret;
+	}
+
+	memset(&address, 0, sizeof(address));
+	address.addr64_enabled = 1;
+	address.addr64[0] = 0x00;
+	address.addr64[1] = 0x00;
+	address.addr64[2] = 0x00;
+	address.addr64[3] = 0x00;
+	address.addr64[4] = 0x00;
+	address.addr64[5] = 0x00;
+	address.addr64[6] = 0xFF;
+	address.addr64[7] = 0xFF;
+	if ((ret = xbee_conNew(xbee, &con, "64-bit Data", &address)) != XBEE_ENONE) {
+		xbee_log(xbee, -1, "xbee_conNew() returned: %d (%s)", ret, xbee_errorToStr(ret));
+		return ret;
+	}
+
+	if ((ret = xbee_conCallbackSet(con, myCB, NULL)) != XBEE_ENONE) {
+		xbee_log(xbee, -1, "xbee_conCallbackSet() returned: %d", ret);
+		return ret;
+	}
+
+	/* getting an ACK for a broadcast message is kinda pointless... */
+	xbee_conSettings(con, NULL, &settings);
+	settings.disableAck = 1;
+	xbee_conSettings(con, &settings, NULL);
+
+	for (i = 0; i < 1000; i++) {
+		xbee_conTx(con, NULL, "%d", i);
+
+		/* XBee Series 1 modules don't use meshing, so you can broadcast much faster than Series 2 */
+		usleep(10000); /* 10ms */
+	}
+
+	if ((ret = xbee_conEnd(con)) != XBEE_ENONE) {
+		xbee_log(xbee, -1, "xbee_conEnd() returned: %d", ret);
+		return ret;
+	}
+
+	xbee_shutdown(xbee);
+
+	return 0;
+}
