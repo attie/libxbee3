50a514bf7a88885263a306fe090093fcfcaa9392
Attie Grande <attie@attie.co.uk>
2012-03-05 18:44:27 +0000

    fixed memory leak in network server

--- net.c
+++ net.c
@@ -113,6 +113,7 @@ xbee_err xbee_netClientFree(struct xbee_netClientInfo *info) {
 	xbee_frameBlockFree(info->fBlock);
 	xbee_txFree(info->iface.tx);
 	xbee_rxFree(info->iface.rx);
+	ll_free(info->conList, xbee_conEnd);
 	
 	free(info);
 	return XBEE_ENONE;
