a089b356c3e019c08ae19d495786d6be12c587a2
Attie Grande <attie@attie.co.uk>
2012-03-08 21:24:01 +0000

    fixed install after adding make_manual.sh

--- libconfig.mk
+++ libconfig.mk
@@ -15,8 +15,8 @@ SRCS:=          ll ver prepare xbee pkt conn error frame mode thread log tx rx \
 MODELIST:=      xbee1 xbee2 net
 
 SYS_HEADERS=    xbee.h
-SYS_MANPAGES=   $(shell find $(MANDIR) -type f)
-SYS_MANLINKS=   $(shell find $(MANDIR) -type l)
+SYS_MANPAGES=   $(shell find $(MANDIR) -type f -name '*.3')
+SYS_MANLINKS=   $(shell find $(MANDIR) -type l -name '*.3')
 
 RELEASE_FILES=  HISTORY LICENSE README
 INSTALL_FILES=  $(addprefix $(SYS_INCDIR)/,$(SYS_HEADERS)) \
