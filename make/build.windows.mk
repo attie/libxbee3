4f25cd480b37a25b03c8f2a25e340babf5eea92e
Attie Grande <attie@attie.co.uk>
2012-04-08 18:30:02 +0100

    BROKEN: fixed build system

--- make/build.windows.mk
+++ make/build.windows.mk
@@ -12,24 +12,24 @@ $(DESTDIR)/$(LIBNAME)$(LIBMAJ).dll: .$(DESTDIR).dir $(CORE_OBJS) $(MODE_OBJS) $(
 # dynamically generate these rules for each mode
 define mode_rule
 # build a mode's object
-$$(BUILDDIR)/modes_$1_%.o: .$$(BUILDDIR).dir
+$$(BUILDDIR)/modes_$1_%.o: .$$(BUILDDIR).dir modes/$1/%.c
 	$$(CC) $$(CFLAGS) modes/$1/$$*.c /c /Fo$$@
 endef
 $(foreach mode,$(MODELIST),$(eval $(call mode_rule,$(mode))))
 #####
 
 # build the common mode code
-$(BUILDDIR)/modes_%.o: .$(BUILDDIR).dir
+$(BUILDDIR)/modes_%.o: .$(BUILDDIR).dir modes/%.c
 	$(CC) $(CFLAGS) modes/$*.c /c /Fo$@
 
 ###
 # these objects require special treatment
-$(BUILDDIR)/ver.o: $(BUILDDIR)/%.o: .$(BUILDDIR).dir
+$(BUILDDIR)/ver.o: $(BUILDDIR)/%.o: .$(BUILDDIR).dir %.c
 	$(CC) $(CFLAGS) $(VER_DEFINES) $*.c /c /Fo$@
-$(BUILDDIR)/mode.o: $(BUILDDIR)/%.o: .$(BUILDDIR).dir
+$(BUILDDIR)/mode.o: $(BUILDDIR)/%.o: .$(BUILDDIR).dir %.c
 	$(CC) $(CFLAGS) /DMODELIST="$(addsuffix $(COMMA),$(addprefix &mode_,$(MODELIST))) NULL" $*.c /c /Fo$@
 #####
 
 # build a core object
-$(BUILDDIR)/%.o: .$(BUILDDIR).dir
+$(BUILDDIR)/%.o: .$(BUILDDIR).dir %.c
 	$(CC) $(CFLAGS) $*.c /c /Fo$@
