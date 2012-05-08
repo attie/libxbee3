41e5642d248787c284023c5b631dbd5d7e2de355
Attie Grande <attie@attie.co.uk>
2012-05-08 22:14:02 +0100

    imported common functionality from xbee2 => xbeeZB

--- modes/xbeeZB/mode.c
+++ modes/xbeeZB/mode.c
@@ -25,9 +25,17 @@
 
 #include "../../internal.h"
 #include "../../xbee_int.h"
+#include "../../log.h"
 #include "../../mode.h"
+#include "../../frame.h"
+#include "../../pkt.h"
 #include "../common.h"
 #include "mode.h"
+#include "at.h"
+#include "data.h"
+#include "dataExp.h"
+#include "io.h"
+#include "identify.h"
 
 static xbee_err init(struct xbee *xbee, va_list ap);
 static xbee_err mode_shutdown(struct xbee *xbee);
@@ -35,16 +43,138 @@ static xbee_err mode_shutdown(struct xbee *xbee);
 /* ######################################################################### */
 
 static xbee_err init(struct xbee *xbee, va_list ap) {
-	return XBEE_ENOTIMPLEMENTED;
+	xbee_err ret;
+	char *t;
+	struct xbee_modeData *data;
+	
+	if (!xbee) return XBEE_EMISSINGPARAM;
+	
+	if ((data = malloc(sizeof(*data))) == NULL) return XBEE_ENOMEM;
+	memset(data, 0, sizeof(*data));
+	xbee->modeData = data;
+	
+	ret = XBEE_ENONE;
+	
+	/* currently I don't see a better way than this - using va_arg()... which is gross */	
+	t = va_arg(ap, char*);
+	if ((data->serialInfo.device = malloc(strlen(t) + 1)) == NULL) { ret = XBEE_ENOMEM; goto die; }
+	strcpy(data->serialInfo.device, t);
+	
+	data->serialInfo.baudrate = va_arg(ap, int);
+	
+	if ((ret = xsys_serialSetup(&data->serialInfo)) != XBEE_ENONE) goto die;
+	
+	return XBEE_ENONE;
+die:
+	mode_shutdown(xbee);
+	return ret;
 }
 
 static xbee_err mode_shutdown(struct xbee *xbee) {
-	return XBEE_ENOTIMPLEMENTED;
+	struct xbee_modeData *data;
+	
+	if (!xbee) return XBEE_EMISSINGPARAM;
+	if (!xbee->mode || !xbee->modeData) return XBEE_EINVAL;
+	
+	data = xbee->modeData;
+	
+	xsys_serialShutdown(&data->serialInfo);
+	if (data->serialInfo.device) free(data->serialInfo.device);
+	if (data->serialInfo.txBuf) free(data->serialInfo.txBuf);
+	free(xbee->modeData);
+	xbee->modeData = NULL;
+	
+	return XBEE_ENONE;
 }
 
 /* ######################################################################### */
 
+xbee_err xbee_sZB_transmitStatus_rx_func(struct xbee *xbee, void *arg, unsigned char identifier, struct xbee_buf *buf, struct xbee_frameInfo *frameInfo, struct xbee_conAddress *address, struct xbee_pkt **pkt) {
+	xbee_err ret;
+	
+	if (!xbee || !frameInfo || !buf || !address || !pkt) return XBEE_EMISSINGPARAM;
+	
+	ret = XBEE_ENONE;
+	
+	if (buf->len != 7) {
+		ret = XBEE_ELENGTH;
+		goto die1;
+	}
+	
+#warning TODO - currently missing out on the resolved network address, retry count, and discovery status
+	frameInfo->active = 1;
+	frameInfo->id = buf->data[1];
+	frameInfo->retVal = buf->data[5];
+	
+	goto done;
+die1:
+done:
+	return ret;
+}
+
+/* ######################################################################### */
+
+struct xbee_modeDataHandlerRx xbee_sZB_transmitStatus_rx  = {
+	.identifier = 0x8B,
+	.func = xbee_sZB_transmitStatus_rx_func,
+};
+struct xbee_modeConType xbee_sZB_transmitStatus = {
+	.name = "Transmit Status",
+	.allowFrameId = 1,
+	.useTimeout = 0,
+	.addressRules = ADDR_NONE,
+	.rxHandler = &xbee_sZB_transmitStatus_rx,
+	.txHandler = NULL,
+};
+
+/* ######################################################################### */
+
+xbee_err xbee_sZB_modemStatus_rx_func(struct xbee *xbee, void *arg, unsigned char identifier, struct xbee_buf *buf, struct xbee_frameInfo *frameInfo, struct xbee_conAddress *address, struct xbee_pkt **pkt) {
+	struct xbee_pkt *iPkt;
+	xbee_err ret;
+
+	if (!xbee || !frameInfo || !buf || !address || !pkt) return XBEE_EMISSINGPARAM;
+	
+	if (buf->len != 2) return XBEE_ELENGTH;
+	
+	if ((ret = xbee_pktAlloc(&iPkt, NULL, 1)) != XBEE_ENONE) return ret;
+	
+	iPkt->dataLen = 1;
+	iPkt->data[0] = buf->data[1];
+	iPkt->data[iPkt->dataLen] = '\0';
+	
+	*pkt = iPkt;
+	
+	return 0;
+}
+
+/* ######################################################################### */
+
+struct xbee_modeDataHandlerRx xbee_sZB_modemStatus_rx  = {
+	.identifier = 0x8A,
+	.func = xbee_sZB_modemStatus_rx_func,
+};
+struct xbee_modeConType xbee_sZB_modemStatus = {
+	.name = "Modem Status",
+	.allowFrameId = 0,
+	.useTimeout = 0,
+	.addressRules = ADDR_NONE,
+	.rxHandler = &xbee_sZB_modemStatus_rx,
+	.txHandler = NULL,
+};
+
+/* ######################################################################### */
+
 static const struct xbee_modeConType *conTypes[] = {
+	&xbee_sZB_transmitStatus,
+	&xbee_sZB_modemStatus,
+	&xbee_sZB_localAt,
+	&xbee_sZB_remoteAt,
+	&xbee_sZB_data,
+	&xbee_sZB_dataExp,
+	&xbee_sZB_io,
+	/*&xbee_sZB_sensor,*/
+	&xbee_sZB_identify,
 	NULL
 };
 
