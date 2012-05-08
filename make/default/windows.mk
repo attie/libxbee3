47c1282fda91f6e7f15b424a61b813406162436e
Attie Grande <attie@attie.co.uk>
2012-05-08 22:57:55 +0100

    added MODELIST definition example to defconfig for windows too

--- make/default/windows.mk
+++ make/default/windows.mk
@@ -4,6 +4,9 @@
 VCPATH:=C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC
 SDKPATH:=C:\Program Files\Microsoft SDKs\Windows\v6.0A
 
+### using this can create a smaller binary, by removing modes you won't use
+#MODELIST:=             xbee1 xbee2
+
 ### debug options
 #DEBUG:=/Zi /MTd
 #LDBUG:=/DEBUG
