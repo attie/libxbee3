attie/libxbee3

commit 8626aaf6c83b989e062d8bb27e9431032a2c0bb9

enhanced newCon() address checking

modes/xbee2/at.c
@@ -28,7 +28,6 @@
 #include "../../pkt.h"
 #include "../../frame.h"
 #include "../common.h"
-#include "../addrval.h"
 #include "at.h"
 #include "io.h"
 
@@ -90,7 +89,6 @@ xbee_err xbee_s2_at_tx_func(struct xbee *xbee, struct xbee_con *con, void *arg, 
 	struct xbee_buf *iBuf;
 	size_t bufLen;
 	unsigned char *addr16;
-	unsigned char *addr64;
 	int pos;
 	size_t memSize;
 	
@@ -102,25 +100,21 @@ xbee_err xbee_s2_at_tx_func(struct xbee *xbee, struct xbee_con *con, void *arg, 
 	switch (identifier) {
 		case 0x08: /* Local AT */
 			if (settings->queueChanges) identifier = 0x09;
-			addr64 = NULL;
 			addr16 = NULL;
 			break;
 		case 0x17: /* Remote AT */
-			if (address->addr64_enabled) {
-				addr64 = &(address->addr64[0]);
-				addr16 = (unsigned char[]){ 0xFF, 0xFE };
-			} else if (address->addr16_enabled) {
-				addr64 = (unsigned char[]){ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+			if (!address->addr64_enabled) return XBEE_EINVAL;
+			if (address->addr16_enabled) {
 				addr16 = &(address->addr16[0]);
-			} else {
-				return XBEE_EINVAL;
+			} else if (address->addr16_enabled) {
+				addr16 = (unsigned char[]){ 0xFF, 0xFE };
 			}
 			break;
 		default: return XBEE_EINVAL;
 	}
 	
 	memSize = 2;
-	if (addr64 && addr16) memSize += 8 + 2;
+	if (addr16) memSize += 8 + 2;
 	memSize += len;
 	bufLen = memSize;
 	
@@ -132,8 +126,8 @@ xbee_err xbee_s2_at_tx_func(struct xbee *xbee, struct xbee_con *con, void *arg, 
 	iBuf->len = bufLen;
 	iBuf->data[pos] = identifier;                          pos++;
 	iBuf->data[pos] = frameId;                             pos++;
-	if (addr64 && addr16) {
-		memcpy(&(iBuf->data[pos]), addr64, 8);               pos += 8;
+	if (addr16) {
+		memcpy(&(iBuf->data[pos]), address->addr64, 8);      pos += 8;
 		memcpy(&(iBuf->data[pos]), addr16, 2);               pos += 2;
 		iBuf->data[pos] = 0x00;
 		if (!settings->queueChanges) {
@@ -170,7 +164,7 @@ const struct xbee_modeConType xbee_s2_localAt = {
 		.tv_sec = 0,
 		.tv_nsec = 250000000,
 	},
-	.address_validator = xbee_addrval_none,
+	.addressRules = ADDR_NONE,
 	.rxHandler = &xbee_s2_localAt_rx,
 	.txHandler = &xbee_s2_localAt_tx,
 };
@@ -193,7 +187,7 @@ const struct xbee_modeConType xbee_s2_remoteAt = {
 		.tv_sec = 0,
 		.tv_nsec = 750000000,
 	},
-	.address_validator = xbee_addrval_16or64,
+	.addressRules = ADDR_64_16OPT_NOEP,
 	.rxHandler = &xbee_s2_remoteAt_rx,
 	.txHandler = &xbee_s2_remoteAt_tx,
 };

modes/xbee1/at.c
@@ -28,7 +28,6 @@
 #include "../../pkt.h"
 #include "../../frame.h"
 #include "../common.h"
-#include "../addrval.h"
 #include "at.h"
 #include "io.h"
 
@@ -170,7 +169,7 @@ const struct xbee_modeConType xbee_s1_localAt = {
 		.tv_sec = 0,
 		.tv_nsec = 250000000,
 	},
-	.address_validator = xbee_addrval_none,
+	.addressRules = ADDR_NONE,
 	.rxHandler = &xbee_s1_localAt_rx,
 	.txHandler = &xbee_s1_localAt_tx,
 };
@@ -193,7 +192,7 @@ const struct xbee_modeConType xbee_s1_remoteAt = {
 		.tv_sec = 0,
 		.tv_nsec = 750000000,
 	},
-	.address_validator = xbee_addrval_16or64,
+	.addressRules = ADDR_16XOR64_NOEP,
 	.rxHandler = &xbee_s1_remoteAt_rx,
 	.txHandler = &xbee_s1_remoteAt_tx,
 };

conn.c
@@ -327,9 +327,16 @@ xbee_err _xbee_conNew(struct xbee *xbee, struct xbee_interface *interface, int a
 	if ((ret = xbee_modeLocateConType(interface->conTypes, allowInternal, type, NULL, NULL, &conType)) != XBEE_ENONE) return ret;
 	if (!conType) return XBEE_EUNKNOWN;
 	
-	if (conType->address_validator) {
-		if ((ret = conType->address_validator(address)) != XBEE_ENONE) return ret;
-	}
+	xbee_log(-1, "%02X", conType->addressRules);
+	
+	if (conType->addressRules & ADDR_EP_NOTALLOW && ( address &&  address->endpoints_enabled))                          return XBEE_EINVAL;
+	if (conType->addressRules & ADDR_EP_REQUIRED && (!address || !address->endpoints_enabled))                          return XBEE_EINVAL;
+	if (conType->addressRules & ADDR_64_NOTALLOW && ( address &&  address->addr64_enabled))                             return XBEE_EINVAL;
+	if (conType->addressRules & ADDR_16_NOTALLOW && ( address &&  address->addr16_enabled))                             return XBEE_EINVAL;
+	if (conType->addressRules & ADDR_64_REQUIRED && (!address || !address->addr64_enabled))                             return XBEE_EINVAL;
+	if (conType->addressRules & ADDR_16_REQUIRED && (!address || !address->addr16_enabled))                             return XBEE_EINVAL;
+	if (conType->addressRules & ADDR_16OR64      && (!address || !(address->addr16_enabled | address->addr64_enabled))) return XBEE_EINVAL;
+	if (conType->addressRules & ADDR_16XOR64     && (!address || !(address->addr16_enabled ^ address->addr64_enabled))) return XBEE_EINVAL;
 	
 	conIdentifier = 0;
 	if (xbee->mode->support.conNew) {

mode.h
@@ -59,9 +59,44 @@ struct xbee_modeConType {
 	const unsigned char useTimeout   : 1;
 	const struct timespec timeout;
 	
-	xbee_err (*address_validator)(struct xbee_conAddress *address);
 	const unsigned char save_addr16;
 	const unsigned char save_addr64;
+	
+	
+#define ADDR_EP_NOTALLOW    0x01
+#define ADDR_EP_REQUIRED    0x02
+#define ADDR_64_NOTALLOW    0x04
+#define ADDR_16_NOTALLOW    0x08
+#define ADDR_64_REQUIRED    0x10
+#define ADDR_16_REQUIRED    0x20
+#define ADDR_16OR64         0x40
+#define ADDR_16XOR64        0x80
+
+#define ADDR_NONE           (ADDR_16_NOTALLOW | ADDR_64_NOTALLOW | ADDR_EP_NOTALLOW)
+
+#define ADDR_16_ONLY        (ADDR_16_REQUIRED | ADDR_64_NOTALLOW | ADDR_EP_NOTALLOW)
+#define ADDR_64_ONLY        (ADDR_16_NOTALLOW | ADDR_64_REQUIRED | ADDR_EP_NOTALLOW)
+#define ADDR_EP_ONLY        (ADDR_16_NOTALLOW | ADDR_64_NOTALLOW | ADDR_EP_REQUIRED)
+
+#define ADDR_64_16OPT_EP    (ADDR_64_REQUIRED | ADDR_EP_REQUIRED)
+#define ADDR_64_16OPT_NOEP  (ADDR_64_REQUIRED | ADDR_EP_NOTALLOW)
+#define ADDR_64_16OPT_EPOPT (ADDR_64_REQUIRED)
+
+#define ADDR_16OR64_NOEP    (ADDR_16OR64 | ADDR_EP_NOTALLOW)
+#define ADDR_16OR64_EP      (ADDR_16OR64 | ADDR_EP_REQUIRED)
+#define ADDR_16XOR64_NOEP   (ADDR_16XOR64 | ADDR_EP_NOTALLOW)
+#define ADDR_16XOR64_EP     (ADDR_16XOR64 | ADDR_EP_REQUIRED)
+	/* 0b........
+	     -------1  - endpoints not allowed
+	     ------1-  - endpoints required
+	     -----1--  - 64-bit not allowed
+	     ----1---  - 16-bit not allowed
+	     ---1----  - 64-bit address required
+	     --1-----  - 16-bit address required
+	     -1------  - 16 or 64-bit address required (OR)
+	     1-------  - 16 or 64-bit address required (XOR) */
+	const unsigned char addressRules;
+	
 	
 	const struct xbee_modeDataHandlerRx *rxHandler;
 	const struct xbee_modeDataHandlerTx *txHandler;

modes/addrval.c
@@ -1,63 +1,0 @@
-/*
-	libxbee - a C library to aid the use of Digi's XBee wireless modules
-	          running in API mode.
-
-	Copyright (C) 2009 onwards  Attie Grande (attie@attie.co.uk)
-
-	libxbee is free software: you can redistribute it and/or modify it
-	under the terms of the GNU Lesser General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	libxbee is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Lesser General Public License for more details.
-
-	You should have received a copy of the GNU Lesser General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "../internal.h"
-#include "addrval.h"
-
-xbee_err xbee_addrval_none(struct xbee_conAddress *address) {
-	/* we don't need any address information */
-	if (!address) return XBEE_ENONE;
-	if (address->addr16_enabled) return XBEE_EINVAL;
-	if (address->addr64_enabled) return XBEE_EINVAL;
-	if (address->endpoints_enabled) return XBEE_EINVAL;
-	return XBEE_ENONE;
-}
-
-xbee_err xbee_addrval_16or64(struct xbee_conAddress *address) {
-	/* we need only ONE of 16-bit, or 64-bit address, NOT both */
-	if (!address) return XBEE_EINVAL;
-	if (address->endpoints_enabled) return XBEE_EINVAL;
-	if (!address->addr16_enabled && !address->addr64_enabled) return XBEE_EINVAL; /* neither */
-	if ( address->addr16_enabled &&  address->addr64_enabled) return XBEE_EINVAL; /* both */
-	return XBEE_ENONE;
-}
-
-/* ######################################################################### */
-
-xbee_err xbee_addrval_16bit(struct xbee_conAddress *address) {
-	/* we need only need 16-bit info */
-	if (!address) return XBEE_EINVAL;
-	if (!address->addr16_enabled) return XBEE_EINVAL;
-	if (address->addr64_enabled) return XBEE_EINVAL;
-	if (address->endpoints_enabled) return XBEE_EINVAL;
-	return XBEE_ENONE;
-}
-
-xbee_err xbee_addrval_64bit(struct xbee_conAddress *address) {
-	/* we need only need 64-bit info */
-	if (!address) return XBEE_EINVAL;
-	if (address->addr16_enabled) return XBEE_EINVAL;
-	if (!address->addr64_enabled) return XBEE_EINVAL;
-	if (address->endpoints_enabled) return XBEE_EINVAL;
-	return XBEE_ENONE;
-}

modes/addrval.h
@@ -1,29 +1,0 @@
-#ifndef __XBEE_MODE_ADDRVAL_H
-#define __XBEE_MODE_ADDRVAL_H
-
-/*
-	libxbee - a C library to aid the use of Digi's XBee wireless modules
-	          running in API mode.
-
-	Copyright (C) 2009 onwards  Attie Grande (attie@attie.co.uk)
-
-	libxbee is free software: you can redistribute it and/or modify it
-	under the terms of the GNU Lesser General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	libxbee is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Lesser General Public License for more details.
-
-	You should have received a copy of the GNU Lesser General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-xbee_err xbee_addrval_none(struct xbee_conAddress *address);
-xbee_err xbee_addrval_16or64(struct xbee_conAddress *address);
-xbee_err xbee_addrval_16bit(struct xbee_conAddress *address);
-xbee_err xbee_addrval_64bit(struct xbee_conAddress *address);
-
-#endif /* __XBEE_MODE_ADDRVAL_H */

modes/makefile
@@ -2,7 +2,7 @@ include ../config.mk
 include ../buildconfig.mk
 
 
-SRCS:=common addrval
+SRCS:=common
 
 
 PDEPS:=makefile ../config.mk ../buildconfig.mk

modes/xbee1/data.c
@@ -27,7 +27,6 @@
 #include "../../mode.h"
 #include "../../pkt.h"
 #include "../common.h"
-#include "../addrval.h"
 #include "data.h"
 
 xbee_err xbee_s1_data_rx_func(struct xbee *xbee, void *arg, unsigned char identifier, struct xbee_buf *buf, struct xbee_frameInfo *frameInfo, struct xbee_conAddress *address, struct xbee_pkt **pkt) {
@@ -137,7 +136,7 @@ const struct xbee_modeConType xbee_s1_16bitData = {
 	.name = "16-bit Data",
 	.allowFrameId = 1,
 	.useTimeout = 0,
-	.address_validator = xbee_addrval_16bit,
+	.addressRules = ADDR_16_ONLY,
 	.rxHandler = &xbee_s1_16bitData_rx,
 	.txHandler = &xbee_s1_16bitData_tx,
 };
@@ -156,7 +155,7 @@ const struct xbee_modeConType xbee_s1_64bitData = {
 	.name = "64-bit Data",
 	.allowFrameId = 1,
 	.useTimeout = 0,
-	.address_validator = xbee_addrval_64bit,
+	.addressRules = ADDR_64_ONLY,
 	.rxHandler = &xbee_s1_64bitData_rx,
 	.txHandler = &xbee_s1_64bitData_tx,
 };

commit cc41da1c61876d07b34d39091ee820dafd71c9aa

fixed bug in xbee2 remote AT connections - introduced in 8626aaf6c83b989e062d8bb27e9431032a2c0bb9

modes/xbee2/at.c
@@ -106,7 +106,7 @@ xbee_err xbee_s2_at_tx_func(struct xbee *xbee, struct xbee_con *con, void *arg, 
 			if (!address->addr64_enabled) return XBEE_EINVAL;
 			if (address->addr16_enabled) {
 				addr16 = &(address->addr16[0]);
-			} else if (address->addr16_enabled) {
+			} else {
 				addr16 = (unsigned char[]){ 0xFF, 0xFE };
 			}
 			break;