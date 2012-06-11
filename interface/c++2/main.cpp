987164d29abd7b5bb737d3c7b01b4fa8cc2af429
Attie Grande <attie@attie.co.uk>
2012-06-11 23:41:12 +0100

    tidied main.cpp

--- interface/c++2/main.cpp
+++ interface/c++2/main.cpp
@@ -2,6 +2,8 @@
 
 #include "libxbee.h"
 
+/* ========================================================================== */
+
 class myConnection: public libxbee::ConCallback {
 	public:
 		explicit myConnection(libxbee::XBee &parent, std::string type);
@@ -19,40 +21,23 @@ void myConnection::xbee_conCallback(struct xbee_pkt **pkt, void **data) {
 	std::cout << "\n";
 }
 
-void a(libxbee::XBee &xbee) {
-	libxbee::Con con(xbee, "Local AT");
-	sleep(2);
-	con.Tx("NI");
-	sleep(2);
-}
-void b(libxbee::XBee &xbee) {
-	libxbee::Con *con;
-	
-	con = new libxbee::Con(xbee, "Local AT");
-	sleep(2);
-	con->Tx("NI");
-	sleep(2);
-	delete con;
-}
-void c(libxbee::XBee &xbee) {
-	myConnection con(xbee, "Local AT");
-	sleep(2);
-	con.Tx("NI");
-	sleep(2);
-}
+/* ========================================================================== */
 
 int main(int argc, char *argv[]) {
-	libxbee::XBee xbee("xbee1", "/dev/ttyUSB0", 57600);
-
 	try {
-		//a(xbee);
-		//b(xbee);
-		c(xbee);
+		/* setup libxbee */
+		libxbee::XBee xbee("xbee1", "/dev/ttyUSB0", 57600);
+
+		/* make a connection */
+		myConnection con(xbee, "Local AT");
+
+		/* send data */
+		con.Tx("NI");
+
+		sleep(1);
 	} catch (xbee_err err) {
 		std::cout << "Error " << err << "\n";
 	}
-	
-	sleep(2);
 	
 	return 0;
 }
