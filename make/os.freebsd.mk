778f4e956fb7d6c62ff49af3dfeeb2c82c82446a
Attie Grande <attie@attie.co.uk>
2012-06-14 22:15:21 +0100

    updated freebsd build system to support C++ interface

--- make/os.freebsd.mk
+++ make/os.freebsd.mk
@@ -4,6 +4,7 @@ INSTALL_RULES:=        unix.mk
 
 AS=                    $(CROSS_COMPILE)as
 GCC=                   $(CROSS_COMPILE)gcc
+GXX=                   $(CROSS_COMPILE)g++
 LD=                    $(CROSS_COMPILE)ld
 OBJCOPY=               $(CROSS_COMPILE)objcopy
 AR=                    $(CROSS_COMPILE)ar
@@ -18,30 +19,43 @@ DEBUG:=                -g
 CFLAGS+=               -Wall -c -fPIC $(DEBUG) $(addprefix -D,$(OPTIONS))
 #CFLAGS+=              -pedantic
 CFLAGS+=               -fvisibility=hidden
+CFLAGS+=               -Wno-variadic-macros
 CPPFLAGS:=             $(CFLAGS)
-CFLAGS+=               -Wstrict-prototypes -Wno-variadic-macros
+CFLAGS+=               -Wstrict-prototypes
 CLINKS+=               $(addprefix -l,$(LIBS)) $(DEBUG)
 
-LIB_OUT:=              $(DESTDIR)/$(LIBNAME).so \
-                       $(DESTDIR)/$(LIBNAME).a
+LIB_OUT:=              $(DESTDIR)/$(LIBNAME).so  \
+                       $(DESTDIR)/$(LIBNAME).a   \
+                       $(DESTDIR)/$(LIBNAME)p.so \
+                       $(DESTDIR)/$(LIBNAME)p.a
 
 INSTALL_FILES=         $(SYS_LIBDIR)/$(LIBNAME).so.$(LIBFULLREV)                    \
                        $(SYS_LIBDIR)/$(LIBNAME).so.$(LIBFULLREV).dbg                \
                        $(SYS_LIBDIR)/$(LIBNAME).so                                  \
                        $(SYS_LIBDIR)/$(LIBNAME).a.$(LIBFULLREV)                     \
                        $(SYS_LIBDIR)/$(LIBNAME).a                                   \
+                       $(SYS_LIBDIR)/$(LIBNAME)p.so.$(LIBFULLREV)                   \
+                       $(SYS_LIBDIR)/$(LIBNAME)p.so.$(LIBFULLREV).dbg               \
+                       $(SYS_LIBDIR)/$(LIBNAME)p.so                                 \
+                       $(SYS_LIBDIR)/$(LIBNAME)p.a.$(LIBFULLREV)                    \
+                       $(SYS_LIBDIR)/$(LIBNAME)p.a                                  \
                        $(addprefix $(SYS_MANDIR)/,$(addsuffix .gz,$(SYS_MANPAGES))) \
-                       $(SYS_INCDIR)/xbee.h
+                       $(SYS_INCDIR)/xbee.h                                         \
+                       $(SYS_INCDIR)/xbeep.h
 
-RELEASE_FILES=         $(DESTDIR)/$(LIBNAME).so.$(LIBFULLREV)     \
-                       $(DESTDIR)/$(LIBNAME).so.$(LIBFULLREV).dbg \
-                       $(DESTDIR)/$(LIBNAME).so                   \
-                       $(DESTDIR)/$(LIBNAME).a.$(LIBFULLREV)      \
-                       $(DESTDIR)/$(LIBNAME).a                    \
-                       $(addprefix $(MANDIR)/,$(SYS_MANPAGES))    \
-                       xbee.h                                     \
-                       interface/c++/xbeep.cpp                    \
-                       interface/c++/xbeep.h                      \
+RELEASE_FILES=         $(DESTDIR)/$(LIBNAME).so.$(LIBFULLREV)      \
+                       $(DESTDIR)/$(LIBNAME).so.$(LIBFULLREV).dbg  \
+                       $(DESTDIR)/$(LIBNAME).so                    \
+                       $(DESTDIR)/$(LIBNAME).a.$(LIBFULLREV)       \
+                       $(DESTDIR)/$(LIBNAME).a                     \
+                       $(DESTDIR)/$(LIBNAME)p.so.$(LIBFULLREV)     \
+                       $(DESTDIR)/$(LIBNAME)p.so.$(LIBFULLREV).dbg \
+                       $(DESTDIR)/$(LIBNAME)p.so                   \
+                       $(DESTDIR)/$(LIBNAME)p.a.$(LIBFULLREV)      \
+                       $(DESTDIR)/$(LIBNAME)p.a                    \
+                       $(addprefix $(MANDIR)/,$(SYS_MANPAGES))     \
+                       xbee.h                                      \
+                       xbeep.h                                     \
                        README HISTORY COPYING COPYING.LESSER
 
 CLEAN_FILES=           $(BUILDDIR)/*.o \
