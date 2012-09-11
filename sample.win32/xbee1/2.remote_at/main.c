9ea9d4cd8555217da910339d0630bc103beb0710
Attie Grande <attie@attie.co.uk>
2012-09-11 23:56:08 +0100

    added a note about Windows COM port addressing

--- sample.win32/xbee1/2.remote_at/main.c
+++ sample.win32/xbee1/2.remote_at/main.c
@@ -40,6 +40,9 @@ int main(void) {
 	char txRet;
 	xbee_err ret;
 
+	/* if this doesn't work, try something like the following:
+		xbee_setup(&xbee, "xbee1", "\\\\.\\COM25", 57600);
+	*/
 	if ((ret = xbee_setup(&xbee, "xbee1", "COM8", 57600)) != XBEE_ENONE) {
 		printf("ret: %d (%s)\n", ret, xbee_errorToStr(ret));
 		return ret;
