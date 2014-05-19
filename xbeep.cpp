270b60f8ac7c03142171f1ac43ea14a909df2889
Attie Grande <attie@attie.co.uk>
2014-05-19 08:51:03 +0100

    improved(?) ConCallback::libxbee_callbackFunction - will now perform better, but is a little less 'safe'

--- xbeep.cpp
+++ xbeep.cpp
@@ -319,27 +319,20 @@ EXPORT libxbee::ConCallback::ConCallback(XBee &parent, std::string type, struct 
 }
 
 void libxbee::ConCallback::libxbee_callbackFunction(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, void **data) {
-	std::list<XBee*>::iterator i;
-	for (i = xbeeList.begin(); i != xbeeList.end(); i++) {
-		if ((*i)->getHnd() != xbee) continue;
-
-		Con *c;
-		if ((c = (*i)->conLocate(con)) == NULL) break;
-
-		Pkt *pktClass = new Pkt(*pkt);
-
-		c->xbee_conCallback(&pktClass);
-
-		/* if they took the packet, then don't free/delete it */
-		if (pktClass != NULL) {
-			delete pktClass;
-		}
-
-		/* either way, libxbee doesn't need to free it, it was free'd just now or its the user's responsibility... */
-		*pkt = NULL;
+	Con *c = (libxbee::Con*)*data;
+	if (c->parent.getHnd() != xbee) {
+		std::cerr << " -1#[" << __FILE__ << ":" << __LINE__ << "] " << __FUNCTION__ << "(): A connection called back to the C++ interface, but it didn't provide suitable information...\n";
 		return;
 	}
-	std::cerr << "  1#[" << __FILE__ << ":" << __LINE__ << "] " << __FUNCTION__ << "(): A connection called back to the C++ interface, but it wasnt found...\n";
+
+	Pkt *p = new Pkt(*pkt);
+	*pkt = NULL; /* we'll either delete its container right after the callback, or it becomes the user's responsibility */
+
+	c->xbee_conCallback(&p);
+
+	if (p != NULL) {
+		delete p;
+	}
 }
 
 /* ========================================================================== */
